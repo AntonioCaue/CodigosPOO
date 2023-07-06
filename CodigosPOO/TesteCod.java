0ecd5cfbcf4cbadf54abb6af052ce3690e14ded5
ANTONIO CAUE OLIVEIRA MORAIS <92269446+AntonioCaue@users.noreply.github.com>
2023-07-06 09:32:08 -0300

    Delete CodigosPOO directory

--- CodigosPOO/TesteCod.java
+++
@@ -1,9 +0,0 @@
-public class TesteCod {
-    public static void main(String[] args){
-        for (int i = 1; i <= 10; i++) {
-            if (i == 3) continue;
-            if (i == 8) break;
-            System.out.println(i);
-            }
-    }
-}
